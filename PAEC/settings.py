f898f8b257c095d5d86073fd45e6ff395dcf39fd
cristian23398 <cristian23398@cbtis75.edu.mx>
2025-11-28 04:25:37 -0600

    M13

--- PAEC/settings.py
+++ PAEC/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5kcue7q2nhk9pm2ut&@0kx7-5w!b(3p=yb1d=*(mv_1=+)$&4)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
