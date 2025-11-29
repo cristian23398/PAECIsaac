c5d899e5f22b7f391b98b6db5f0fbe214a4b2bbd
cristian23398 <cristian23398@cbtis75.edu.mx>
2025-11-28 18:30:33 -0600

    M22

--- PAEC/settings.py
+++ PAEC/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
